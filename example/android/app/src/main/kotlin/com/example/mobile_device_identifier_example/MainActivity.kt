6edbcda6d5cb4aaee836748666d8561bb395f5c1
Muhammed Zeyrek <m.zeyrek@muditatech.com>
2024-06-11 09:55:14 +0300

    android upgrade

--- example/android/app/src/main/kotlin/com/example/mobile_device_identifier_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/mobile_device_identifier_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.mobile_device_identifier_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
